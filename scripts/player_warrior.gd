70de652c894861d57beb24f656453d6122c2580c
resasa1 <resaprasetyo17@gmail.com>
2026-06-11 14:59:49 +0700

    add: obstacle and stairs in tilemaps

--- scripts/player_warrior.gd
+++ scripts/player_warrior.gd
@@ -6,6 +6,10 @@ const SPEED = 200.0
 
 var is_attacking: bool = false
 var total_coin: int = 0
+var audio_tween: Tween
+
+@onready var run_sound = $run_sound
+@onready var attack_sound = $attack_sound
 
 @onready var coin_modal = $CoinModal
 @onready var coin_label = $CoinModal/CoinLabel
@@ -35,9 +39,19 @@ func _physics_process(delta: float) -> void:
 		velocity = direction * SPEED
 		
 		animated_sprite.play("run") 
+		
+		if not run_sound.playing:
+				run_sound.play()
+				
+		if audio_tween and audio_tween.is_valid():
+			audio_tween.kill() # Hentikan proses fade-out yang sedang berjalan
+		run_sound.volume_db = 0.0
 	else:
 		velocity = Vector2.ZERO
 		animated_sprite.play("idle") 
+		
+		if run_sound.playing and (audio_tween == null or not audio_tween.is_valid()):
+			fade_out_run_sound()
 
 	move_and_slide()
 	
@@ -46,9 +60,19 @@ func _physics_process(delta: float) -> void:
 	elif direction_x < 0:
 		animated_sprite.flip_h = true   
 
+func fade_out_run_sound() -> void:
+	audio_tween = create_tween()
+	audio_tween.tween_property(run_sound, "volume_db", -40.0, 0.2)
+	audio_tween.tween_callback(run_sound.stop)
+
 func start_attack() -> void:
 	is_attacking = true
 	animated_sprite.play("attack")
+	if audio_tween and audio_tween.is_valid():
+		audio_tween.kill()
+	run_sound.stop()
+	
+	attack_sound.play()
 	
 	if not animated_sprite.animation_finished.is_connected(_on_attack_animation_finished):
 		animated_sprite.animation_finished.connect(_on_attack_animation_finished)
